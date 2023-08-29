12fd435d3e3494dd76af4ac473e1a294f4373183
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-08-29 19:25:31 +0500

    fixed big shot area

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ class Settings:
 
         # настройки пуль
         self.bullet_speed = 1.0
-        self.bullet_width = 500
+        self.bullet_width = 5
         self.bullet_height = 25
         self.bullet_colour = (60, 200, 200)
         self.bullets_allowed = 3
